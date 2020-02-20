f093632fef42f7ae8457f87ec1742e1981a5d346
mar-jene <m.mar.jene@gmail.com>
2020-02-20 16:02:15 +0100

    creating Group entity

--- src/main/java/org/udg/pds/springtodo/entity/Group.java
+++ src/main/java/org/udg/pds/springtodo/entity/Group.java
@@ -1,7 +1,30 @@
 package org.udg.pds.springtodo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.security.Signature;
 
-@Entity
-public class Group {
+@Entity(name="usergroup")
+public class Group implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String description;
+
+    public Group(){
+    }
+    public Group(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
 }
